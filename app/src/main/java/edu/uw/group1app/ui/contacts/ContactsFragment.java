54d76d90654e37e4c543cbbf105eace04a7c087d
Tai Nguyen <tainguyen2101@gmail.com>
2020-11-02 16:53:47 -0800

    Added Fragments inside of Contacts

--- app/src/main/java/edu/uw/group1app/ui/contacts/ContactsFragment.java
+++ app/src/main/java/edu/uw/group1app/ui/contacts/ContactsFragment.java
@@ -8,8 +8,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.uw.group1app.R;
+import edu.uw.group1app.ui.home.HomeFragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +26,8 @@ import edu.uw.group1app.R;
  * @version 1.0
  */
 public class ContactsFragment extends Fragment {
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     public ContactsFragment() {
         //Constructor.
@@ -33,5 +44,58 @@ public class ContactsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_contacts, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager = view.findViewById(R.id.pager);
+
+        //attach tablayout with viewpager
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager(), 1);
+        // add your fragments
+        adapter.addFrag(new HomeFragment(), "Recents");
+        adapter.addFrag(new HomeFragment(), "All");
+        adapter.addFrag(new HomeFragment(), "Favorite");
+
+        viewPager.setAdapter(adapter);
+
+
+    }
 }
+class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    public void addFrag(Fragment fragment) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add("");
+    }
+
+    public void addFrag(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+}
+
