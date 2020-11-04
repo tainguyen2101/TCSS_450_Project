35af057f036d2baee5c684d5123ab63e3187f1ee
Ivan <mendei@uw.edu>
2020-11-03 19:19:33 -0800

    set up: sign in fragment, signinviewmodel, auth_graph, io package, and model package. adjusted manifest to start with auth activity and added permissions

--- app/src/main/java/edu/uw/group1app/AuthActivity.java
+++ app/src/main/java/edu/uw/group1app/AuthActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AuthenticationActivity extends AppCompatActivity {
+public class AuthActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_authentication);
+        setContentView(R.layout.activity_auth);
     }
 }
