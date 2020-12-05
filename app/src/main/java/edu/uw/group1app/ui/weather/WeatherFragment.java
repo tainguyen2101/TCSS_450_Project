9d21ff22598a078fe2a49783259d2c9dc1e6b1a3
mendei <mendei@uw.edu>
2020-12-05 15:24:30 -0800

    added status and temp for zip code 98374

--- app/src/main/java/edu/uw/group1app/ui/weather/WeatherFragment.java
+++ app/src/main/java/edu/uw/group1app/ui/weather/WeatherFragment.java
@@ -2,13 +2,24 @@ package edu.uw.group1app.ui.weather;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import edu.uw.group1app.R;
+import edu.uw.group1app.databinding.FragmentCurrentWeatherBinding;
+import edu.uw.group1app.databinding.FragmentWeatherBinding;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,50 +27,53 @@ import edu.uw.group1app.R;
  */
 public class WeatherFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public WeatherFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WeatherFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WeatherFragment newInstance(String param1, String param2) {
-        WeatherFragment fragment = new WeatherFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private CurrentWeatherViewModel mModel;
+    private FragmentWeatherBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        mModel = new ViewModelProvider(getActivity()).get(CurrentWeatherViewModel.class);
+        mModel.connect();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+        binding = FragmentWeatherBinding.inflate(inflater, container, false);
+        return  binding.getRoot();
     }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+        //FragmentWeatherBinding binding = FragmentWeatherBinding.bind(getView());
+
+        mModel.addResponseObserver(getViewLifecycleOwner(), this::observeResponse);
+
+
+    }
+
+   private void observeResponse(final JSONObject response){
+
+        try {
+            binding.textViewStatus.setText(response.getString("WeatherText"));
+
+            JSONObject temp = response.getJSONObject("Temperature");
+            JSONObject type = temp.getJSONObject("Imperial");
+            binding.textViewTemperature.setText(type.getString("Value"));
+
+        } catch(JSONException e){
+            Log.e("JSON Parse Error",e.getMessage());
+        }
+
+    }
+
+
+
 }
