3b1d4d2cfd335ed57441a3f5901a0606416915d8
mendei <mendei@uw.edu>
2020-12-10 17:37:19 -0800

    5dayhome working for all 3 options. working on 12hour and 12hourhome but template is set up. working on 5day, but template is *now* set up. weather on home fragment

--- app/src/main/java/edu/uw/group1app/ui/home/HomeFragment.java
+++ app/src/main/java/edu/uw/group1app/ui/home/HomeFragment.java
@@ -15,17 +15,36 @@ import android.view.ViewGroup;
 
 import com.pusher.pushnotifications.PushNotifications;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import edu.uw.group1app.R;
 import edu.uw.group1app.databinding.FragmentHomeBinding;
+import edu.uw.group1app.model.PushyTokenViewModel;
 import edu.uw.group1app.model.UserInfoViewModel;
+import edu.uw.group1app.ui.signin.SignInViewModel;
+import edu.uw.group1app.ui.weather.CurrentWeatherViewModel;
+import edu.uw.group1app.ui.weather.ZipcodeViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+    private CurrentWeatherViewModel mWeatherModel;
+    private ZipcodeViewModel mZipModel;
+
+
     public HomeFragment() {
 
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWeatherModel = new ViewModelProvider(getActivity()).get(CurrentWeatherViewModel.class);
+        mZipModel = new ViewModelProvider(getActivity()).get(ZipcodeViewModel.class);
+        mZipModel.connect("98374");
     }
 
     @Override
@@ -38,10 +57,39 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         UserInfoViewModel model = new ViewModelProvider(getActivity())
                 .get(UserInfoViewModel.class);
         FragmentHomeBinding.bind(getView()).emailLabel.setText("Hello " + model.getEmail());
 
+
+
+        mZipModel.addResponseObserver(getViewLifecycleOwner(), response ->{
+            try {
+
+                mWeatherModel.connect(response.getString("Key"));
+                FragmentHomeBinding.bind(getView()).textViewHomeWeatherCity.setText(
+                        response.getString("LocalizedName")
+                );
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        });
+
+        mWeatherModel.addResponseObserver(getViewLifecycleOwner(), response ->{
+            try {
+                FragmentHomeBinding.bind(getView()).textViewHomeWeatherCond.setText(
+                        response.getString("WeatherText"));
+                JSONObject temp = response.getJSONObject("Temperature");
+                JSONObject type = temp.getJSONObject("Imperial");
+                FragmentHomeBinding.bind(getView()).textViewHomeWeatherTemp.setText(
+                        type.getString("Value"));
+                FragmentHomeBinding.bind(getView()).textViewHomeWeatherUnit.setText("F");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        });
     }
 
     @Override
